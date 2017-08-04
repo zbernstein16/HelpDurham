d5ba84578ae6b413af58b22affa5f634e4d6b5b9
Zachary Bernstein <ZacharyBernstein@Zacharys-MBP.fios-router.home>
2017-08-04 13:08:44 -0400

    MongoDB test

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var express = require('express');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://heroku_b7frswz9:j8florr8sljd2rde6dtiilf861@ds135963.mlab.com:35963/heroku_b7frswz9";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
