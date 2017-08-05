d60248348d48ad2926e0ed7ff0a8cc938283daa8
Zachary Bernstein <ZacharyBernstein@Zacharys-MBP.fios-router.home>
2017-08-05 00:26:06 -0400

    New Commi

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://heroku_b7frswz9:j8florr8sljd2rde6dtiilf861@ds135963.mlab.com:35963/heroku_b7frswz9";
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://heroku_b7frswz9:j8florr8sljd2rde6dtiilf861@ds135963.mlab.com:35963/heroku_b7frswz9");
+
+var UserSchema = new mongoose.Schema({
+  name:String,
+  address:String,
+  updated_at: {type:Date, default:Date.now},
+});
+
+var User = mongoose.model('User',UserSchema);
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var myObj = { "name":"Zachary", address:"Highway 37"};
+/*  db.collection("Customers").insertOne(myObj,function(err,res) {
+      if (err) throw err;
+      console.log("One user inserted");
+      db.close();
+  }); */
 });
 
 var app = express();
@@ -20,7 +32,16 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+
+
+  var user = new User({name:'Zach',address:'17 Hill'});
+  user.save(function(error) {
+      if(error)
+        console.log(error);
+      else
+        console.log(user);
+  });
+
 });
 
 app.listen(app.get('port'), function() {
